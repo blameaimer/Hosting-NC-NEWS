189a10a752a053d6c6ca8e52118cf895142b7e94
blameaimer <blameaimer@gmail.com>
2022-02-14 15:26:28 +0000

    Finished ticket #7

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -13,7 +13,7 @@ afterAll(() => {
 
 
 describe("/api/topics", () => {
-    describe("GET", () => {});
+    describe("GET", () => {
     test("should return an object containing all slugs and descriptions ", () => {
       return request(app)
         .get("/api/topics")
@@ -39,11 +39,11 @@ describe("/api/topics", () => {
     .then((response) => {
       expect(response.error.text).toBe('Path not found!');
     });
-    
+})
   });
   });
   describe("/api/articles/articleid", () => {
-    describe("GET", () => {});
+    describe("GET", () => {
     test("should return an object containing a particular topic ", () => {
         return request(app)
           .get("/api/articles/1")
@@ -61,7 +61,7 @@ describe("/api/topics", () => {
                 })
           });
       });
-      test('this is a test which requests an invalid article id and expects a 400 response', () => {
+      test('this is a test which requests an invalid articleId surrounded by quotation marks and expects a 400 response', () => {
           return request(app)
           .get("/api/articles/'notanid'")
           .expect(400)    
@@ -88,4 +88,62 @@ describe("/api/topics", () => {
           expect(msg).toBe('No topic found for id: 69');
         });
     });
+    });
+    describe('PATCH', () => {
+      test('status:200, responds with the updated article', () => {
+      const newVote = {
+        vote: 19
+      };
+
+      return request(app)
+        .patch('/api/articles/3')
+        .send(newVote)
+        .expect(200)
+        .then((response) => {
+          expect(response.body.article.votes).toEqual(19);
+        });
+    });
+    test('same test but with negative integer', () => {
+        const anotherVote = {
+            vote : -200
+        }
+        return request(app)
+        .patch('/api/articles/4')
+        .send(anotherVote)
+        .expect(200)
+        .then((response) => {
+          expect(response.body.article.votes).toEqual(-200);
+        });
+    });
+   
+    test('this is a test where the given vote is not a number', () => {
+        const anotherVote = {
+            vote : "fds"
+        }
+        return request(app)
+        .patch('/api/articles/4')
+        .send(anotherVote)
+        .expect(400)
+        .then((response) => {
+            const {msg} = response._body
+           expect(msg).toBe('Bad Request');
+         });
+        
+    });
+    test('test for invalid article id handling', () => {
+
+        const newVote = {
+            vote: 19
+          };
+        return request(app)
+        .patch('/api/articles/252525')
+        .send(newVote)
+        .expect(404)
+        .then((response) => {
+            const {msg} = response._body
+           expect(msg).toBe('No topic found for id: 252525')
+         });
+        
+    });
   });
+})
--- app.js
+++ app.js
@@ -1,13 +1,25 @@
 const express = require("express");
 const app = express();
 
-const {getTopics,getArticleById} = require('./controllers/topic-controller')
+const {getTopics,getArticleById,patchArticleById} = require('./controllers/topic-controller')
 const {handleInvalidPaths,handlePSQLErrors,handleServerErrors,handleCustomErrors} = require('./errors/index')
 
+
+
 app.use(express.json());
+
+//GETS
 app.get("/api/topics", getTopics)
 app.get("/api/articles/:article_id",getArticleById)
+//POSTS
 
+//PATCHS
+app.patch("/api/articles/:article_id",patchArticleById)
+
+
+
+
+//ERRORS
 app.use("/api/*",handleInvalidPaths)
 app.use(handleCustomErrors)
 app.use(handlePSQLErrors)
