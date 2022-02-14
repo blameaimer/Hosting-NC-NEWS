c689c537ebb4fe9edb9f3e0d9301b7bc8cfb620e
blameaimer <blameaimer@gmail.com>
2022-02-14 11:26:16 +0000

    Finishing #2 ticket /api/topics endpoint

---
+++ models/topic-model.js
@@ -0,0 +1,11 @@
+const db = require("../db/connection");
+
+exports.selectTopics = ()=>{
+
+    return db
+    .query(`SELECT * FROM topics`
+    )
+    .then(({ rows }) => {
+      return rows;
+    });
+}
