31e83a9515aa5ab4dea9b1f1398ab09c3063677a
blameaimer <blameaimer@gmail.com>
2022-02-14 11:53:11 +0000

    Seperated app tests from the utils test file

--- __tests__/utils.test.js
+++ __tests__/utils.test.js
@@ -4,18 +4,7 @@ const {
   createRef,
   formatComments,
 } = require("../db/helpers/utils");
-const request = require("supertest");
-const app = require('../app')
-const db = require("../db/connection");
-const testData = require('../db/data/test-data');
-const seed = require('../db/seeds/seed');
 
-//TESTS ------------------------------------------------------
-beforeEach(() => seed(testData)); // seeding test data before each test
-afterAll(() => {
-    if (db.end) db.end(); // after tests are done ending db connection
-  });
-//
 describe("convertTimestampToDate", () => {
   test("returns a new object", () => {
     const timestamp = 1557572706232;
@@ -115,33 +104,4 @@ describe("formatComments", () => {
   });
 });
 
-describe("/api/topics", () => {
-  describe("GET", () => {});
-  test("should return an object containing all slugs and descriptions ", () => {
-    return request(app)
-      .get("/api/topics")
-      .expect(200)
-      .then((response) => {
-        expect(response.body.topics).toHaveLength(3);
-        response.body.topics.forEach((topic) => {
-          expect(topic).toEqual(
-            {
-              slug: expect.any(String),
-              description: expect.any(String),
-            })
-          
-        });
-      });
-  });
-});
-describe('Invalid path error test', () => {
-  test.only('this should response with a 404 status and a msg body', () => {
-  return request(app)
-  .get("/api/teatasd")
-  .expect(404)  
-  .then((response) => {
-    expect(response.error.text).toBe('Path not found!');
-  });
-  
-});
-});
+
