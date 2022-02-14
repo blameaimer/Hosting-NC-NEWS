5e7afad2cf2bc87818a4457f479309257ec3d6e2
blameaimer <blameaimer@gmail.com>
2022-02-14 14:28:33 +0000

    Finished ticket #14

---
+++ errors/index.js
@@ -0,0 +1,16 @@
+exports.handleInvalidPaths=(req,res)=>{
+    res.status(404).send('Path not found!');
+}
+exports.handleCustomErrors = (err, req, res, next) => {
+    if (err.status && err.msg) {
+      res.status(err.status).send({ msg: err.msg });
+    } else next(err);
+  };
+exports.handlePSQLErrors=(err, req, res, next) => {
+    if (err.code === '22P02'||err.code ==='42703') {
+      res.status(400).send({ msg: 'Bad Request' });
+    } else next(err);
+  };
+  exports.handleServerErrors = (err, req, res, next) => {
+    res.status(500).send({ msg: 'Internal Server Error' });
+  };
