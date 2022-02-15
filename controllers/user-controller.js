a0e178927e64fbc6aab92beca7ae9dd34857ff61
blameaimer <blameaimer@gmail.com>
2022-02-15 11:13:55 +0000

    Seperating functions and making changes as suggested by Dave

---
+++ controllers/user-controller.js
@@ -0,0 +1,10 @@
+const {selectUsers} = require("../models/user-model")
+
+exports.getUsers = (req, res, next) => {
+    selectUsers()
+      .then((users) => res.status(200).send({ users }))
+      .catch((err) => {
+        next(err);
+      });
+  };
+  
