2bf53678ca3d9271ba8e827cf9d18f1b9b43e6fc
blameaimer <blameaimer@gmail.com>
2022-02-18 11:16:08 +0000

    Done changes requested by Dave

--- app.js
+++ app.js
@@ -2,25 +2,12 @@ const express = require("express");
 const app = express();
 
 const {handleInvalidPaths,handlePSQLErrors,handleServerErrors,handleCustomErrors} = require('./errors/index')
-const {getUsers} = require('./controllers/user-controller')
-const{getCommentsByArticleId,postComment,removeCommentById} = require('./controllers/comment-controller')
+
 const apiRouter = require('./routes/api-router');
 app.use(express.json());
 
 //GETS
-app.get("/api",apiRouter)
-app.get("/api/topics", apiRouter)
-app.get("/api/articles",apiRouter)
-app.get("/api/articles/:article_id",apiRouter)
-app.get("/api/users",apiRouter)
-app.get("/api/articles/:article_id/comments",apiRouter)
-//POSTS
-app.post("/api/articles/:article_id/comments",apiRouter)
-//DELETES
-app.delete("/api/comments/:comment_id", apiRouter);
-//PATCHS
-app.patch("/api/articles/:article_id",apiRouter)
-
+app.use("/api",apiRouter)
 
 
 
