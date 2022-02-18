474f7338eab60431650bbbf59939d653ed0efe2f
blameaimer <blameaimer@gmail.com>
2022-02-18 11:12:51 +0000

    Started setting up ticket #23

--- routes/comments-router.js
+++ routes/comments-router.js
@@ -2,6 +2,6 @@ const commentRouter = require('express').Router();
 const{removeCommentById,updateComment} = require('../controllers/comment-controller')
 commentRouter.route('/:comment_id')
 .delete(removeCommentById)
-.patch(updateComment)
+// .patch(updateComment)
 
 module.exports = commentRouter;
