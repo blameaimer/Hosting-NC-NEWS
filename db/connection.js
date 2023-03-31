0c588278f85bf4fdfc58655c5e42dfc608917a35
blameaimer <blameaimer@gmail.com>
2023-03-31 12:59:15 +0100

    Updated API Host

--- db/connection.js
+++ db/connection.js
@@ -1,20 +1,21 @@
-const { Pool } = require('pg');
-const ENV = process.env.NODE_ENV || 'development';
+const { Pool } = require("pg");
+const ENV = process.env.NODE_ENV || "development";
 const config =
-  ENV === 'production'
+  ENV === "production"
     ? {
         connectionString: process.env.DATABASE_URL,
+        max: 2,
         ssl: {
           rejectUnauthorized: false,
         },
       }
     : {};
-require('dotenv').config({
+require("dotenv").config({
   path: `${__dirname}/../.env.${ENV}`,
 });
 
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
-  throw new Error('PGDATABASE or DATABASE_URL not set');
+  throw new Error("PGDATABASE or DATABASE_URL not set");
 }
 
 module.exports = new Pool(config);
